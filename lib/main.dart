3f01686d27aee3d9ceab7cf74392c7b778696e53
jkoob04 <134137248+jkoob04@users.noreply.github.com>
2023-07-19 09:13:38 +0330

    listView_top_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,69 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: SizedBox(
+            height: 45,
+            child: TextField(
+              decoration: InputDecoration(
+                  suffixIcon: Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  ),
+                  hintTextDirection: TextDirection.rtl,
+                  hintText: 'جستجو در همه کالاها',
+                  fillColor: Colors.grey[350],
+                  filled: true),
+            ),
+          ),
+
+          ///hide the titel and icons
+          // title: Text('digikala'),
+          // backgroundColor: Colors.red[700],
+          // actions: [
+          //   IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart_outlined))
+          // ],
+        ),
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            Container(
+              color: Colors.redAccent,
+              height: 150,
+              width: 300,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemCount: 8,
+                itemBuilder: (context, index) => Container(
+                  width: 400,
+                  child: Card(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
 }
